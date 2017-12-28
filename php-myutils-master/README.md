4b22291b64602b31c12d9812c504b4c08ed9831e
robinn00 <robinn00@163.com>
2017-12-28 15:08:02 +0800

    php生成缩略图

---
+++ php-myutils-master/README.md
@@ -0,0 +1,52 @@
+﻿# php-myutils
+## IDValidator 身份证验证
+
+```php
+header("Content-type: text/html; charset=utf-8");
+
+include 'IDValidator.php';
+$v = com\jdk5\blog\IDValidator\IDValidator::getInstance();
+
+//生成一个18位身份证号
+$id = $v->makeID();
+//获取身份证信息
+$info = $v->getInfo($id);
+var_dump($info);
+//生成一个15位身份证号
+$id = $v->makeID(true);
+$info = $v->getInfo($id);
+var_dump($info);
+
+//验证身份证号是否正确
+var_dump($v->isValid("123456789012345678"));
+```
+
+## image 图片处理工具
+
+
+```php
+use com\jdk5\blog\Image\Image;
+require '../Image.php';
+
+$img = new Image();
+
+$watermark = array(
+	"filename" => "watermarkater.png",	//水印文件
+	"position" => self::CENTER,	//水印的位置，分别为:center|top|left|bottom|right|top left|top right|bottom left|bottom right
+	"opacity" => 1,	//水印的透明度，可以为0-1的任意数值，默认为1
+	"x_offset" => 0,	//加水印的x轴偏移量，默认为0
+	"y_offset" => 0,	//加水印的y轴偏移量，默认为0
+	"angle" => self::WATERMARK_DIAGONAL_NEG	//水印的旋转角度，可以为-360-360，如果为WATERMARK_DIAGONAL_POS或WATERMARK_DIAGONAL_NEG，则沿着生成图片的对角线旋转，默认为0
+);
+$img->load('org.jpg')
+	//->width(200)	//设置生成图片的宽度，高度将按照宽度等比例缩放
+	//->height(200)	//设置生成图片的高度，宽度将按照高度等比例缩放
+	->size(300, 300)	//设置生成图片的宽度和高度
+	->fixed_given_size(true)	//生成的图片是否以给定的宽度和高度为准
+	->keep_ratio(true)		//是否保持原图片的原比例
+	->bgcolor("#ffffff")	//设置背景颜色，按照rgb格式
+	->rotate(20)	//指定旋转的角度
+	->quality(50)	//设置生成图片的质量 0-100，如果生成的图片格式为png格式，数字越大，压缩越大，如果是其他格式，如jpg，gif，数组越小，压缩越大
+	->set_watermark($watermark)		//添加水印
+	->save('processed/org-width-resize.jpg');	//保存生成图片的路径
+```
---
+++ php-myutils-master/image/example/index.php
@@ -0,0 +1,65 @@
+<?php
+use com\jdk5\blog\Image\Image;
+require '../Image.php';
+
+$img = new Image();
+
+$img->load('basketball.gif')->size(100, 200)->fixed_given_size(true)->save('processed/basketball-resize.gif');
+
+$img->load('org.jpg')->width(200)->quality(70)->save('processed/org-width-resize.jpg');
+
+$img->load('org.jpg')->height(200)->save('processed/org-height-resize.jpg');
+
+$img->load('org.png')->fixed_given_size(true)->keep_ratio(true)->save('processed/org-no-size-resize.png');
+
+$img->load('org.png')->bg_color("#a30000")->quality(100)->size(300, 300)->fixed_given_size(true)->keep_ratio(true)->save('processed/org-size-resize.png');
+
+$img->load('org.png')->bg_color("#ff00ff")->size(400, 100)->fixed_given_size(true)->keep_ratio(true)->save('processed/400_100.png');
+
+$img->load('org.png')->bg_color("#dd0000")->size(200, 200)->fixed_given_size(true)->keep_ratio(true)->save('processed/200_200.png');
+
+$img->load('org.png')->rotate(45)->bg_color("#ee2300")->size(250, 187)->fixed_given_size(true)->keep_ratio(true)->save('processed/rotate-45.png');
+$img->load('org.png')->rotate(20)->bg_color("#3300ff")->width(250)->fixed_given_size(true)->keep_ratio(true)->quality(90)->save('processed/rotate-20.jpg');
+
+// watermark
+$watermark1 = array(
+			"filename" => 'watermarkater.png',
+			"position" => Image::CENTER,
+			"opacity" => 0.6,
+			"x_offset" => 0, 
+			"y_offset" => 0,
+			"angle" => Image::WATERMARK_DIAGONAL_NEG
+		);
+$img->load('org.png')->set_watermark($watermark1)->save('processed/watermark_diagonal.png');
+
+$watermark2 = array(
+			"filename" => 'watermarkater.png',
+			"position" => Image::CENTER,
+			"opacity" => 0.7
+		);
+$img->load('org.png')->set_watermark($watermark2)->save('processed/watermark.png');
+
+$watermark3 = array(
+			"filename" => 'overlay.png',
+			"position" => Image::CENTER,
+			"opacity" => 0.8
+		);
+$img->load('butterfly.jpg')->set_watermark($watermark3)->save('processed/watermark_handle.png');
+
+header("Content-type: text/html; charset=utf-8");
+echo '<h1 style="text-align: center;">生成图片的结果</h1>';
+$pre = "./processed/";
+if ($handle = opendir($pre)) {
+	while (false !== ($entry = readdir($handle))) {
+		if ($entry != "." && $entry != "..") {
+			list($width, $height) = getimagesize($pre.$entry);
+			$size = filesize($pre.$entry);
+			$size = number_format($size, 0, '', ',');
+			echo "<div style='text-align: center; margin: 10px; font-weight: bold;'>".
+				"<img src='$pre$entry' /><br/>filename: {$entry}<br/>".
+				"{$width} * {$height}px<br/>".
+				"size: {$size} byte</div>";
+		}
+	}
+	closedir($handle);
+}
